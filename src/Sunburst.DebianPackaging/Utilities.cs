f4851042663735736bf380c8c43c5facb7bca670
William Kent <wjk@sunsol.me>
2017-03-14 17:31:28 -0400

    Add Format extension method to Utilities class

--- src/Sunburst.DebianPackaging/Utilities.cs
+++ src/Sunburst.DebianPackaging/Utilities.cs
@@ -9,5 +9,21 @@ namespace Sunburst.DebianPackaging
             bool success = dict.TryGetValue(key, out TValue value);
             return success ? value : default_value;
         }
+
+        public static string Format(this string format, IReadOnlyDictionary<string, string> arguments)
+        {
+            string result = format;
+            result = result.Replace("{{", "{\x01");
+            result = result.Replace("}}", "\x01}");
+
+            foreach (var pair in arguments)
+            {
+                string token = string.Concat("{", pair.Key, "}");
+                result = result.Replace(token, pair.Value);
+            }
+
+            result = result.Replace("\x01", "");
+            return result;
+        }
     }
 }
