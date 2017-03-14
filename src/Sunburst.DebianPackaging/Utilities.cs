c6ef4a5f10917be9a9bfe533ee3a48266a13134a
William Kent <wjk@sunsol.me>
2017-03-14 17:36:23 -0400

    Rename Utilities.TryGetValue method

--- src/Sunburst.DebianPackaging/Utilities.cs
+++ src/Sunburst.DebianPackaging/Utilities.cs
@@ -4,7 +4,7 @@ namespace Sunburst.DebianPackaging
 {
     internal static class Utilities
     {
-        public static TValue GetOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dict, TKey key, TValue default_value)
+        public static TValue TryGetValue<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dict, TKey key, TValue default_value)
         {
             bool success = dict.TryGetValue(key, out TValue value);
             return success ? value : default_value;
