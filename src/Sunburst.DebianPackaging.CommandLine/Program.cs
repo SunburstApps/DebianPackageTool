5c7da9f5fdc81e60c292d52b66e79d02b8e6354a
William Kent <wjk@users.noreply.github.com>
2018-02-04 20:26:49 -0500

    Fix filenames in help output

--- src/Sunburst.DebianPackaging.CommandLine/Program.cs
+++ src/Sunburst.DebianPackaging.CommandLine/Program.cs
@@ -17,8 +17,8 @@ namespace Sunburst.DebianPackaging.CommandLine
                 syntax.ApplicationName = "dotnet deb-tool";
                 syntax.DefineOption("i|input-dir", ref inputDir, "The path to the package source directory (default: current directory)");
                 syntax.DefineOption("o|output-dir", ref outputDir, "Where to create the final .deb files (default ./bin/dpkg)");
-                syntax.DefineOption("override-name", ref overrideName, "Override the package name in the package_config.json file");
-                syntax.DefineOption("override-version", ref overrideVersion, "Override the package version in the package_config.json file");
+                syntax.DefineOption("override-name", ref overrideName, "Override the package name in the debian_config.json file");
+                syntax.DefineOption("override-version", ref overrideVersion, "Override the package version in the debian_config.json file");
             });
         }
     }
