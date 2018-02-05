934490d181f6761171e8bdd65a2f12ea227a9284
William Kent <wjk@users.noreply.github.com>
2018-02-04 20:25:22 -0500

    Add commnand-line syntax parsing to CommandLine

--- src/Sunburst.DebianPackaging.CommandLine/Program.cs
+++ src/Sunburst.DebianPackaging.CommandLine/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.CommandLine;
+using System.IO;
 
 namespace Sunburst.DebianPackaging.CommandLine
 {
@@ -6,7 +8,18 @@ namespace Sunburst.DebianPackaging.CommandLine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputDir = Directory.GetCurrentDirectory();
+            string outputDir = Path.Combine(inputDir, "bin", "dpkg");
+            string overrideName = null, overrideVersion = null;
+
+            ArgumentSyntax.Parse(args, syntax =>
+            {
+                syntax.ApplicationName = "dotnet deb-tool";
+                syntax.DefineOption("i|input-dir", ref inputDir, "The path to the package source directory (default: current directory)");
+                syntax.DefineOption("o|output-dir", ref outputDir, "Where to create the final .deb files (default ./bin/dpkg)");
+                syntax.DefineOption("override-name", ref overrideName, "Override the package name in the package_config.json file");
+                syntax.DefineOption("override-version", ref overrideVersion, "Override the package version in the package_config.json file");
+            });
         }
     }
 }
